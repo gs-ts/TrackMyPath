2719b50a555d2dca3654f8576849eb5abba4e6c5
gts <g.tsepas@thebeat.co>
2021-01-02 17:26:14 +0100

    add ActivityRetainedScoped for RetrievePhotosUseCase

--- app/src/main/java/com/gts/trackmypath/domain/usecase/RetrievePhotosUseCase.kt
+++ app/src/main/java/com/gts/trackmypath/domain/usecase/RetrievePhotosUseCase.kt
@@ -1,6 +1,7 @@
 package com.gts.trackmypath.domain.usecase
 
 import javax.inject.Inject
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -9,6 +10,7 @@ import com.gts.trackmypath.common.Result
 import com.gts.trackmypath.domain.PhotoRepository
 import com.gts.trackmypath.domain.model.Photo
 
+@ActivityRetainedScoped
 class RetrievePhotosUseCase @Inject constructor(private val photoRepository: PhotoRepository) {
 
     suspend operator fun invoke(): Result<List<Photo>> = withContext(Dispatchers.IO) {
